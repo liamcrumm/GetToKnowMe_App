72e2da2f3198c83980c5833ab7c4312f6ec64aaf
liamcrumm <liamcrumm@gmail.com>
2016-08-01 21:09:38 -0400

    initial commit

---
+++ app/src/main/java/com/example/liam/gettoknowme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.liam.gettoknowme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
